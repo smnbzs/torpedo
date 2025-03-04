802df15634338d64737b57c44e2014cd1ec59e61
smnbzs <103945912+smnbzs@users.noreply.github.com>
2025-03-04 14:56:27 +0100

    register.php && database
    A register API és a users table kész.

--- REGISTER/script.js
+++ REGISTER/script.js
@@ -5,7 +5,20 @@ function register() {
     var confirmpassword = document.getElementById("confirmpassword");
 
     if (password.value == confirmpassword.value) {
-        fetch("localhost/api/register.php")
+        fetch("http://localhost/pr/torpedo/api/register", 
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json" 
+                },
+                body: JSON.stringify({"username": username.value, "email": email.value, "password": password.value})
+            }).then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error("Hiba történt a regisztráció során: ", error);
+            })
     } else {
         password.value = "";
         confirmpassword.value = "";
